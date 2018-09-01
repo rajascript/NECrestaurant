b0d504d061cbdeb6a270d97d9835a3f88f2d0a46
Raja Raghav <raja_raghav@hotmail.com>
2018-09-01 05:52:38 +0530

    slack github hooks

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var keys = require("./config/keys");
 var app = express();
-//
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
