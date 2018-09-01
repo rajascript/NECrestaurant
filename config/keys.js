12472087730fd15d6dbc84ce63180355fd9e4c49
Raja Raghav <raja_raghav@hotmail.com>
2018-09-01 14:08:43 +0530

    added env vars

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "ci") {
 	module.exports = require("./prod");
 } else {
 	module.exports = require("./dev");
--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,5 @@
 module.exports = {
 	//enter heroku keys here like...
-	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	mongooseURI: process.env.MONGOOSE_URI,
 	cookieKey: process.env.COOKIE_KEY
 };
