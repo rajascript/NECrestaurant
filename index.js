43ddc189ba8e75b4bb3724128e0b046c925b9ae5
Raja Raghav <raja_raghav@hotmail.com>
2018-09-01 05:47:25 +0530

    slack github hooks

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//rotes conf
+
 require("./routes/users")(app);
 
 app.use(express.static(path.join(__dirname, "public")));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
-//server code done here.
+
