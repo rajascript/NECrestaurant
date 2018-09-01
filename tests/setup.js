a7280a32f6831eff841c450b10bb66f090e7c291
Raja Raghav <raja_raghav@hotmail.com>
2018-09-01 16:08:21 +0530

    fixing travis ci

--- tests/setup.js
+++ tests/setup.js
@@ -1,12 +1,9 @@
 jest.setTimeout(30000);
 
-require("../models/User");
+require("../model/User");
 
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(
-	keys.mongoURI,
-	{ useMongoClient: true }
-);
+mongoose.connect(keys.mongooseURI);
