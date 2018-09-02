15a264b05d4f0802cd3e2753541266a023e58518
Raja Raghav <raja_raghav@hotmail.com>
2018-09-02 12:20:32 +0530

    added google oauth

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,7 @@
 module.exports = {
 	mongooseURI: process.env.MONGOOSE_URI,
 	cookieKey: process.env.COOKIE_KEY,
-	testHost: process.env.TEST_HOST
+	testHost: process.env.TEST_HOST,
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 };
