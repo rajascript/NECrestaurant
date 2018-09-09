4fb6df82121647f2eeef89c968c85d3fc2abc226
Codescode <ayushjindal15030@gmail.com>
2018-09-10 00:37:29 +0530

    this too.

--- client/src/Component/Header/SignupForm.js
+++ client/src/Component/Header/SignupForm.js
@@ -3,44 +3,32 @@ import LoginGoogle from "./LoginGoogle";
 import { connect } from "react-redux";
 import { signup } from "../../Action/index";
 class SignupForm extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			email: "",
-			passwordValue: "",
-			confirmPasswordValue: "",
-			nameValue: "",
-			phoneValue: ""
-		};
-		this.handleEmailChange = this.handleEmailChange.bind(this);
-		this.handlePasswordChange = this.handlePasswordChange.bind(this);
-		this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(
-			this
-		);
-		this.handleNameChange = this.handleNameChange.bind(this);
-		this.handlePhoneChange = this.handlePhoneChange.bind(this);
-		this.handleSignup = this.handleSignup.bind(this);
-	}
-	handleSignup(e) {
-		console.log(this.state);
-		let user = {
-			email: this.state.email,
-			password: this.state.passwordValue,
-			name: this.state.nameValue,
-			phone: this.state.phoneValue
-		};
-		this.props.signup(user);
-		e.preventDefault();
-	}
-	handleEmailChange(e) {
-		this.setState({ email: e.target.value });
-	}
-	handleNameChange(e) {
-		this.setState({ nameValue: e.target.value });
-	}
-	handlePhoneChange(e) {
-		this.setState({ phoneValue: e.target.value });
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailValue: "",
+      passwordValue: "",
+      confirmPasswordValue: "",
+      nameValue: "",
+      phoneValue: ""
+    };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(
+      this
+    );
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handlePhoneChange = this.handlePhoneChange.bind(this);
+    this.handleSignup = this.handleSignup.bind(this);
+  }
+  handleSignup(e) {
+    console.log(this.state);
+    let user = {
+      email: this.state.emailValue,
+      password: this.state.passwordValue,
+      name: this.state.nameValue,
+      phone: this.state.phoneValue
+    };
 
     if (
       performPhoneCheck(Number(this.state.phoneValue)) &&
