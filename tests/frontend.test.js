c73177cf325959d708793ae5e03ad4e3e21dded1
agarwalhansika <hanshika96@gmail.com>
2018-09-11 21:27:44 +0530

    merging changes with Raja

--- tests/frontend.test.js
+++ tests/frontend.test.js
@@ -15,3 +15,8 @@ test("Header works", async () => {
 	const text = await page.getContentsOf("p");
 	expect(text).toEqual("I am a header.");
 });
+
+test("Browser", async () => {
+	const check = await page.performExecution("navigator.userAgent");
+	console.log(check);
+});
