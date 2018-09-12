0f37612e4666dc2905cb126a39a297d71de06e48
agarwalhansika <hanshika96@gmail.com>
2018-09-12 13:36:56 +0530

    tested Browser support

--- tests/frontend.test.js
+++ tests/frontend.test.js
@@ -16,7 +16,20 @@ test("Header works", async () => {
 	expect(text).toEqual("Indique"); //edited by hansika. achievement unlocked.
 });
 
-test("Browser", async () => {
+test.only("Browser support", async () => {
 	const check = await page.performExecution("navigator.userAgent");
-	console.log(check);
+	var ua= check, tem, M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    var x = Number(M[1]);
+    expect(x).toBeGreaterThanOrEqual(38);
+    console.log(x);     //Browser support tested.
 });
