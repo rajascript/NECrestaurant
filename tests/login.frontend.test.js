32af63bf56a3b9904e81c7467b0f31c88d9a8692
agarwalhansika <hanshika96@gmail.com>
2018-09-16 23:57:25 +0530

    merge  changes from all

--- tests/login.frontend.test.js
+++ tests/login.frontend.test.js
@@ -11,22 +11,28 @@ afterEach(async () => {
 	await page.close();
 });
 
-test("loginForm check", async () => {
+test("login success", async () => {
     await page.click('#headerButtonLogin');
     await page.waitForSelector('#loginFormContainer');
     await page.click("input[id=loginFormEmail]");
-    await page.type("input[id=loginFormEmail]", "cba@yahoo.com");
+    await page.type("input[id=loginFormEmail]", "abc@yahoo.com");
 	await page.click("input[id=loginFormPassword]");
     await page.type("input[id=loginFormPassword]", '123qwerty');
 
-await page.click("#submit");
+    await page.click("#submitL");   
 
-/*let correct;
-if(correct = await page.waitFor("#loginPopupError"))
-    expect(correct).toEqual("Error: Wrong Credentials")
-*/
-    
+});
 
-    
+test("login fail", async () => {
+    await page.click('#headerButtonLogin');
+    await page.waitForSelector('#loginFormContainer');
+    await page.click("input[id=loginFormEmail]");
+    await page.type("input[id=loginFormEmail]", "abc@yahoo.com");
+	await page.click("input[id=loginFormPassword]");
+    await page.type("input[id=loginFormPassword]", '123qgfwerty');
+
+    await page.click("#submitL");   
+    const check = await page.getContentsOf("#loginPopupError");
+    expect(check).toEqual("Error: Wrong credentials")
 
 });
--- tests/signup.frontend.test.js
+++ tests/signup.frontend.test.js
@@ -11,7 +11,7 @@ afterEach(async () => {
 	await page.close();
 });
 
-test("signupForm check", async () => {
+test("successful Signup", async () => {
 	await page.click("#headerButtonSignup");
 	await page.waitForSelector("#signupFormContainer");
 	await page.click("input[id=signupFormEmail]");
@@ -25,6 +25,8 @@ test("signupForm check", async () => {
 	await page.click("input[id=signupFormPhone]");
 	await page.type("input[id=signupFormPhone]", "8482592952");
 	await page.click("#submitS");
+	await page.waitForNavigation()
+	//const success = await page.click("#headerButtonLogin")
 });
 
 test("signup fails because email is poorly formatted", async () => {
@@ -45,3 +47,61 @@ test("signup fails because email is poorly formatted", async () => {
 	const errorMessage = await page.getContentsOf("#loginPopupError");
 	expect(errorMessage).toEqual("Email is poorly formatted");
 });
+
+test("signup fails password is short", async () => {
+	await page.click("#headerButtonSignup");
+	await page.waitForSelector("#signupFormContainer");
+	await page.click("input[id=signupFormEmail]");
+	await page.type("input[id=signupFormEmail]", "abc@yahoo.com");
+	await page.click("input[id=signupFormPassword]");
+	await page.type("input[id=signupFormPassword]", "123");
+	await page.click("input[id=signupFormConfirmPassword]");
+	await page.type("input[id=signupFormConfirmPassword]", "123");
+	await page.click("input[id=signupFormName]");
+	await page.type("input[id=signupFormName]", "hansika");
+	await page.click("input[id=signupFormPhone]");
+	await page.type("input[id=signupFormPhone]", "8482592952");
+	await page.click("#submitS");
+	await page.waitForSelector("#loginPopupError");
+	const errorMessage = await page.getContentsOf("#loginPopupError");
+	expect(errorMessage).toEqual("password too short");
+});
+
+test("signup fails password does not match", async () => {
+	await page.click("#headerButtonSignup");
+	await page.waitForSelector("#signupFormContainer");
+	await page.click("input[id=signupFormEmail]");
+	await page.type("input[id=signupFormEmail]", "abc@yahoo.com");
+	await page.click("input[id=signupFormPassword]");
+	await page.type("input[id=signupFormPassword]", "123qwerty");
+	await page.click("input[id=signupFormConfirmPassword]");
+	await page.type("input[id=signupFormConfirmPassword]", "123");
+	await page.click("input[id=signupFormName]");
+	await page.type("input[id=signupFormName]", "hansika");
+	await page.click("input[id=signupFormPhone]");
+	await page.type("input[id=signupFormPhone]", "8482592952");
+	await page.click("#submitS");
+	await page.waitForSelector("#loginPopupError");
+	const errorMessage = await page.getContentsOf("#loginPopupError");
+	expect(errorMessage).toEqual("passwords do not match");
+});
+
+test("signup fails for incorrect phone no.", async () => {
+	await page.click("#headerButtonSignup");
+	await page.waitForSelector("#signupFormContainer");
+	await page.click("input[id=signupFormEmail]");
+	await page.type("input[id=signupFormEmail]", "abc@yahoo.com");
+	await page.click("input[id=signupFormPassword]");
+	await page.type("input[id=signupFormPassword]", "123qwerty");
+	await page.click("input[id=signupFormConfirmPassword]");
+	await page.type("input[id=signupFormConfirmPassword]", "123qwerty");
+	await page.click("input[id=signupFormName]");
+	await page.type("input[id=signupFormName]", "hansika");
+	await page.click("input[id=signupFormPhone]");
+	await page.type("input[id=signupFormPhone]", "84825929525654");
+	await page.click("#submitS");
+	await page.waitForSelector("#loginPopupError");
+	const errorMessage = await page.getContentsOf("#loginPopupError");
+	expect(errorMessage).toEqual("phone number must be atleast 10 digits");
+});
+
