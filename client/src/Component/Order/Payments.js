a0aea9e6fc2354ff185a9f6a2c763668889a4186
Codescode <ayushjindal15030@gmail.com>
2018-09-19 21:12:33 +0530

    changes

--- client/src/Component/Order/Payments.js
+++ client/src/Component/Order/Payments.js
@@ -3,22 +3,22 @@ import StripeCheckout from "react-stripe-checkout";
 import { connect } from "react-redux";
 import * as actions from "../../Action";
 class Payments extends Component {
-	render() {
-		return (
-			<StripeCheckout
-				name={"Indique"}
-				description={"Pay for your order"}
-				amount={this.props.total}
-				currency="INR"
-				token={token => this.props.handleToken(token)}
-				stripeKey={process.env.REACT_APP_STRIPE_KEY}
-			>
-				<a className="waves-effect waves-light btn">Pay</a>
-			</StripeCheckout>
-		);
-	}
+  render() {
+    return (
+      <StripeCheckout
+        name={"Indique"}
+        description={"Pay for your order"}
+        amount={this.props.total}
+        currency="INR"
+        token={token => this.props.handleToken(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <a className="waves-effect waves-light btn">Pay</a>
+      </StripeCheckout>
+    );
+  }
 }
 export default connect(
-	null,
-	actions
+  null,
+  actions
 )(Payments);
